3e0d00ba5d650d3af75fd7bf5601931c054838fa
mohanraj-sece <mmohanraj632@gmail.com>
2023-08-22 22:25:02 +0530

    chainproofV2 added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import {Home , Upload , View , Tutorial , Profile ,Nopage} from './components'
+import Demo from './components/Demo';
+import  { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen">
+      <Toaster />
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/view" element={<View />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/test" element={<Demo />} />
+          <Route path="*" element={<Nopage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
